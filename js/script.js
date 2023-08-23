3f8bf3b1a0a386da84a04767a7d92cea34aea3fc
Rafael Oliveira <rafaeloliveira.ds@outlook.com>
2023-08-22 22:16:21 -0300

    trying to make the rating thing work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form")
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+    
     // selecionando o cartão
     const card = document.querySelector("section.card")
     // alinhando texto ao centro
@@ -16,16 +16,27 @@ form.addEventListener("submit", (e) => {
     // trocando a imagem
     image.style.marginTop = "20px"
     image.src = "./images/illustration-thank-you.svg"
-    // criando paragrafo com a nota
-    const rateNote = document.createElement("p")
-    // add texto
-    rateNote.innerText = `You selected 4 out of 5`
-    // adicionando ao fim da div-head
-    imageDiv.append(rateNote)
-    // adicionando classe rate-text
-    rateNote.classList.add("rate-text")
 
-    const url = location.search
-    console.log(url)
+    const rateExist = document.querySelector("#rate-text")
 
+    if (!rateExist) {
+        // criando paragrafo com a nota
+        const rateNote = document.createElement("p")
+        // add id rate-text
+        rateNote.id = "rate-text"
+        // add texto
+        // rateNote.innerText = `You selected 4 out of 5`
+        // adicionando ao fim da div-head
+        imageDiv.append(rateNote)
+        // adicionando classe rate-text
+        rateNote.classList.add("rate-text")
+        // salvando os parâmetros da url
+        const param = new URLSearchParams(location.search)
+        // extraindo o valor de rating
+        const ratingValue = param.get("rating")
+        rateNote.innerText = `You selected ${ratingValue} out of 5`
+        console.log(ratingValue)
+    }
+
+    e.preventDefault()
 })
