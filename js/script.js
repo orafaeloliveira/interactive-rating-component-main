47785ea6a908085b22493e2ec2fab53930a2c4e5
Rafael Oliveira <rafaeloliveira.ds@outlook.com>
2023-08-23 23:36:19 -0300

    rate note using radio checked

--- js/script.js
+++ js/script.js
@@ -17,26 +17,50 @@ form.addEventListener("submit", (e) => {
     image.style.marginTop = "20px"
     image.src = "./images/illustration-thank-you.svg"
 
-    const rateExist = document.querySelector("#rate-text")
-
-    if (!rateExist) {
-        // criando paragrafo com a nota
-        const rateNote = document.createElement("p")
-        // add id rate-text
-        rateNote.id = "rate-text"
-        // add texto
-        // rateNote.innerText = `You selected 4 out of 5`
-        // adicionando ao fim da div-head
-        imageDiv.append(rateNote)
-        // adicionando classe rate-text
-        rateNote.classList.add("rate-text")
-        // salvando os parâmetros da url
-        const param = new URLSearchParams(location.search)
-        // extraindo o valor de rating
-        const ratingValue = param.get("rating")
-        rateNote.innerText = `You selected ${ratingValue} out of 5`
-        console.log(ratingValue)
+    const rateNote = document.createElement("p")
+    rateNote.id = "rate-text"
+    rateNote.classList.add("rate-text")
+    imageDiv.append(rateNote)
+    const radios = document.getElementsByName("rating")
+     
+    function searchCheck(radio) {
+        if (radio.checked) {
+            const value = radio.value
+            rateNote.innerText = `You selected ${value} out of 5`
+            console.log(value)
+        }
     }
+
+    radios.forEach(searchCheck)
+    
+    // const rateExist = document.querySelector("#rate-text")
+
+    // if (!rateExist) {
+    //     // criando paragrafo com a nota
+    //     const rateNote = document.createElement("p")
+    //     // add id rate-text
+    //     rateNote.id = "rate-text"
+    //     // add texto
+    //     rateNote.innerText = ""
+    //     // rateNote.innerText = `You selected 4 out of 5`
+    //     // adicionando ao fim da div-head
+    //     imageDiv.append(rateNote)
+    //     // adicionando classe rate-text
+    //     rateNote.classList.add("rate-text")
+
+    //     const radios = document.getElementsByName("rating")
+    
+    //     function searchCheck(radio) {
+    //         if (radio.checked) {
+    //             const value = radio.value
+    //             rateNote.innerText = `You selected ${value} out of 5`
+    //             console.log(value)
+    //         }
+    //     }
+
+    //     radios.forEach(searchCheck)
+  
+    // }
 
     e.preventDefault()
 })
