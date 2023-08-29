5056851c62bd2b899a5498366e62f0dfecd4fabf
Rafael Oliveira <rafaeloliveira.ds@outlook.com>
2023-08-29 16:32:45 -0300

    almost finished

--- js/script.js
+++ js/script.js
@@ -1,48 +1,52 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-    // selecionando o cartão
+    // get the whole card
     const card = document.querySelector("section.card");
-    // alinhando texto ao centro
+    // align-text: center
     card.style.textAlign = "center";
-    // selecionando a div cabeçalho
+    // get card-head
     const cardHead = document.getElementById("card-head");
-    // selecionando imagem
+    // get the image inside card-head
     const cardImage = document.getElementById("card-image");
+    // change class from rounded to high-img
     cardImage.classList.replace("rounded", "high-img");
-    // image.classList.remove("rounded")
-    // image.classList.add("high-img")
-    // trocando a imagem
+    // add margin-top to it and change the image source
     cardImage.style.marginTop = "20px";
     cardImage.src = "./images/illustration-thank-you.svg";
 
+    // save the rate-note element
     const existRateNote = document.getElementById("rate-note");
 
+    // if a rate-note already exists remove it
     if (existRateNote) {
         existRateNote.remove();
     }
 
+    // create a new rate-note element with id and class rate-note
     const rateNote = document.createElement("p");
     rateNote.id = "rate-note";
     rateNote.classList.add("rate-note");
+    // adding it in the final of card-head
     cardHead.append(rateNote);
+    // saving the radio buttons
     const radios = document.getElementsByName("rating");
-
+    // function to search the radios values and get the value of radio checked
     function searchCheck(radio) {
         if (radio.checked) {
             const value = radio.value;
             rateNote.innerText = `You selected ${value} out of 5`;
-            console.log(value);
         }
     }
-
+    // looping the radios
     radios.forEach(searchCheck);
 
-    const textTitle = document.getElementById("card-title");
-    textTitle.innerText = "Thank you!";
+    // getting and changing the card-title and card-paragraph
+    const cardTitle = document.getElementById("card-title");
+    cardTitle.innerText = "Thank you!";
     const textParagraph = document.getElementById("card-paragraph");
-    textParagraph.innerText =
-        "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
-
+    textParagraph.innerText = "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
+    form.remove()
+    
     e.preventDefault();
 });
